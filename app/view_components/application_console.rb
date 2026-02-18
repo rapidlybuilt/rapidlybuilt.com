68d611d97d5fda0b1e2bb53bb3e1b1bad7ed92ae
dcunning <31681+dcunning@users.noreply.github.com>
2026-02-18 16:43:10 -0500

    lean on rapidly_built's searches implementation

--- app/view_components/application_console.rb
+++ app/view_components/application_console.rb
@@ -1,9 +1,21 @@
 class ApplicationConsole < RapidlyBuilt::Console::Base
-  def initialize(**kwargs)
-    super(**kwargs)
+  self.search_index_path = :search_index_path
+  self.search_path = :search_path
 
+  def build
     request.middleware.use RequestMiddleware
+    request.middleware.use RequestSearchPolish
 
     integrate UiDocs
+  end
+
+  class RequestSearchPolish < RapidlyBuilt::Request::Middleware::Entry
+    def call
+      return unless request.path == helpers.search_path
+
+      ui.layout.header.left.items.last.css_class = "hidden"
+      ui.layout.subheader.css_class = "hidden"
+      ui.layout.sidebars.first.css_class = "hidden" if ui.layout.sidebars.first.present?
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "search", to: "searches#show"
-
-  # only static because we're pre-rendering in production
-  get "search/index", to: "rapidly_built/searches#index", as: :search_index
+  get "search", to: "rapidly_built/searches#show"
+  get "search/index", to: "rapidly_built/searches#index"
 
   namespace :apps do
     root to: "pages#index"
